ba9aab8a524c7ee99b5b97c987840c33d9856d5e
Todd Yamakawa <tyamakawa@tenstorrent.com>
2025-04-21 15:12:33 -0500

    aerospace/install.sh: Updating.

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ brew install --cask nikitabobko/tap/aerospace
 brew tap FelixKratz/formulae
 brew install borders
 
+# https://nikitabobko.github.io/AeroSpace/guide#a-note-on-mission-control
+defaults write com.apple.dock expose-group-apps -bool true && killall Dock
+
+
